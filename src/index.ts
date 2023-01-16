e634c57261b07f11d65525c588a4529381b6c020
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2023-01-16 12:27:05 -0800

    Union types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-type Employee = {
-  readonly id: number,
-  name: string,
-  retire: (date: Date) => void;
-}
-
-let employee: Employee = {
-  id: 1,
-  name: 'Mosh',
-  retire: (date: Date) => {
-    console.log(date)
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === 'number') {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
   }
 }
+
+kgToLbs(10);
+kgToLbs('10kg');
