16eb043fe44fdaf1d9736ab1ea038d162f166bdd
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2023-01-16 12:51:48 -0800

    Unknown type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-let phone = document.getElementById('phone') as HTMLInputElement;
-phone.value
+function render(document: unknown) {
+  // Narrowing
+  if (typeof document === 'string') {
+    document.toUpperCase();
+  }
+}
