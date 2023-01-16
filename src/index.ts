398cfb1ee23b3ed0f547489d68c2bb1e8ce4f79b
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2023-01-16 12:19:29 -0800

    Objects

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-function calculateTax(income: number, taxYear = 2022): number {
-  if (taxYear < 2022) {
-    return income * 1.2;
+let employee: {
+  readonly id: number,
+  name: string,
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: 'Mosh',
+  retire: (date: Date) => {
+    console.log(date)
   }
-  return income * 1.3;
 }
-
-calculateTax(10_000)
