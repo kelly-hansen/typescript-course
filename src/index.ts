72632796a2e32d60a0b18a04fafd4189eabf935a
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2023-01-16 12:48:20 -0800

    Type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-let speed: number | null = null;
-let ride = {
-  // Nullish coalescing operator
-  speed: speed ?? 30
-}
+let phone = document.getElementById('phone') as HTMLInputElement;
+phone.value
